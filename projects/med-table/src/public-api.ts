73b813f900aaeb3f290a810933086f984be19654
Dmytro Rybachek <Puss.in.Boots@i.ua>
2022-03-28 16:15:17 +0300

    update package.json

--- projects/med-table/src/public-api.ts
+++ projects/med-table/src/public-api.ts
@@ -3,6 +3,10 @@
  */
 
 export { FIELD_TYPES } from './lib/constants/fieldTypes';
+
+export * from './lib/types/form';
+export * from './lib/types/table';
+
 export * from './lib/med-table.service';
 export * from './lib/med-table.component';
 export * from './lib/med-table.module';
