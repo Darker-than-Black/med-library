d6bb2dc4e8de261648e71a880b6aeea36c1fc5f9
Dmytro Rybachek <Puss.in.Boots@i.ua>
2022-03-31 14:56:09 +0300

    fixed table sticky

--- projects/med-table/src/lib/constants/selectors.ts
+++ projects/med-table/src/lib/constants/selectors.ts
@@ -3,6 +3,6 @@ export const APP_SELECTOR = 'med-table';
 export const SELECTOR_AFTER_WEB_COMPONENT = 'div'; // web component doesn't have a height
 export const FOOTER_SELECTOR = 'body footer';
 
-export const TABLE_SELECTOR = `${APP_SELECTOR} ${SELECTOR_AFTER_WEB_COMPONENT}`;
+export const TABLE_SELECTOR = `${APP_SELECTOR} p-table ${SELECTOR_AFTER_WEB_COMPONENT}`;
 export const PAGINATOR_SELECTOR = `${APP_SELECTOR} p-paginator ${SELECTOR_AFTER_WEB_COMPONENT}`;
 export const UP_SCROLL_SELECTOR = `${APP_SELECTOR} .med-table__up-scroll`;
